4128116834c1f54d0cad2d45f0c87b1a53c77fc9
rakibkhanofficial <khanrakib218@gmail.com>
2023-11-05 18:48:52 +0600

    feature: add image gallery

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,12 +1,11 @@
 import React from "react";
+import Imagegallery from "../ui/features/imagegallery";
 
 const MainLayout = () => {
 
     return (
         <div className="bg-white text-black">
-           <div className="text-3xl py-4 px-2 border border-red-500 ">header </div> 
-           <div className=" text-4xl py-20 px-5 flex justify-center items-center border border-green-700">Main Body </div> 
-           <div className="text-4xl py-20 px-5 flex justify-center items-center border border-blue-700">Footer </div> 
+           <Imagegallery/>
         </div>
     );
 };
---
+++ src/components/ui/features/imagegallery/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Imagegallery = () => {
+
+    return(
+        <div>
+            <h1 className=" Text-3xl text-center text-black"> Image Gallery</h1>
+        </div>
+    )
+}
+
+export default Imagegallery;
